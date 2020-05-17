619b3d20733e8ab560fbd7c8935654b1d1f82000
a <a@actzero.ai>
2020-05-16 22:49:44 -0700

    GH Workflow to publish lambda layer

---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+LIB_VERSION=2.0.1
+CHROMIUM_VERSION=79.0.3945.0
+LAYER_NAME='chrome-aws-lambda'
+
+LAYER_VERSION=$(
+  aws lambda publish-layer-version --region "$TARGET_REGION" \
+    --layer-name $LAYER_NAME \
+    --zip-file fileb://chrome-aws-lambda/chrome_aws_lambda.zip \
+    --description "chrome-aws-lambda v${LIB_VERSION} & Chromium v${CHROMIUM_VERSION}" \
+    --query Version \
+    --output text
+)
+
+aws lambda add-layer-version-permission \
+  --region "$TARGET_REGION" \
+  --layer-name $LAYER_NAME \
+  --statement-id sid1 \
+  --action lambda:GetLayerVersion \
+  --principal '*' \
+  --query Statement \
+  --output text \
+  --version-number "$LAYER_VERSION"
